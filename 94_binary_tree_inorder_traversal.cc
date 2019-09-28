a09fe18f5d6c7e2625344aca69e9dc17ce9335fa
john-Ly <cruisej9@yahoo.com>
2019-09-28 20:33:39 +0800

    morris postorder @TODO; flatten list

--- 94_binary_tree_inorder_traversal.cc
+++ 94_binary_tree_inorder_traversal.cc
@@ -10,6 +10,13 @@
 #include <limits>  // LONG_MIN
 using namespace std;
 
+// @NOTE 数据结构利用动画来进行理解
+// time complexity: O(n)  每个节点最多被访问两次(包含回溯的过程)
+// space complexity:
+//  O(h)  树高
+//     O(log n) -- best/avg
+//     O(n) -- worst
+
 // 二叉树左右子树有顺序
 // 前中后序(主要是根节点的顺序) 层次
 // https://www.cnblogs.com/grandyang/p/4251757.html
@@ -74,6 +81,9 @@ void Preorder(BinaryNode<T> *root) {
 	Preorder(root->right);    // Visit right subtree
 }
 
+// https://ghh3809.github.io/2018/08/06/morris-traversal
+// 先序遍历的第一个是根节点, 栈会逐级回溯
+// 从根开始遍历, 然后压栈左节点(方便在对应层级访问右节点)
 template <class T>
 void PreorderNonRecursive(BinaryNode<T> *root) {
     stack<BinaryNode<T>*> st;
@@ -84,6 +94,7 @@ void PreorderNonRecursive(BinaryNode<T> *root) {
             cout << cur->data << ' ';
             cur = cur->left; // 会找到最左边节点
         } else {
+            // 找到最左边节点后 回溯(利用栈)
             auto tmp = st.top(); st.pop();
             cur = tmp->right;
         }
@@ -115,6 +126,7 @@ void Inorder(BinaryNode<T> *root) {
 
 // https://www.cnblogs.com/grandyang/p/4297300.html
 // https://www.cnblogs.com/grandyang/p/4620012.html  返回第k小的元素(二叉树中序)
+// 从根开始遍历, 然后压栈左节点(方便在对应层级访问右节点)
 template <class T>
 void InorderNonRecursive(BinaryNode<T> *root) {
     // int cnt = 0;
@@ -134,6 +146,10 @@ void InorderNonRecursive(BinaryNode<T> *root) {
     }
 }
 
+/*
+ 1. 若节点还有左子树, 先把左子树访问完(符合中序遍历的顺序)
+ 2. 没有左子树, 访问最左节点, 并尝试访问右子树
+ */
 template <class T>
 void InorderNonRecursive2(BinaryNode<T> *root) {
     stack<BinaryNode<T>*> st;
@@ -149,6 +165,80 @@ void InorderNonRecursive2(BinaryNode<T> *root) {
         }
     }
 }
+
+/*
+  十分详细的morris遍历
+  https://www.cnblogs.com/AnnieKim/archive/2013/06/15/morristraversal.html
+  https://ghh3809.github.io/2018/08/06/morris-traversal/
+
+ - 从根节点开始访问。
+ - 如果当前节点C不存在左子树，按中序遍历的规则，应当访问节点C，并进入其右子树进行遍历。
+ - 如果当前节点C存在左子树，就找到C的前驱节点B，并将B的右孩子指向C（存储后继），同时当前节点转入左子树进行遍历。
+ - 步骤2中访问右子树时，如果节点本身没有右子树，则会直接转入其后继节点C。根据中序遍历的规则，说明此时C的左子树遍历完成。为了还原树结构，我们需要重新找到C的前驱节点，并将其右孩子设置为nullptr。之后我们访问节点C，并进入其右子树进行遍历。
+
+ 后继节点的计算方法(中序遍历转换)
+ 1. 对于存在右子树的节点A，其后继节点是其右子树中最左侧的节点；
+ 2. 对于没有右子树的节点B，其后继节点是其自下而上的父节点中第一个将其作为左子树的节点。
+
+ 虽然直接后继不好判断 可以通过根节点的左子树来判断是否是前驱(反向判断)
+
+
+ 时间复杂度为 O(n) 仅可能两次查找前驱节点
+ */
+template <class T>
+vector<T> InorderMorris(BinaryNode<T> *root) {
+    if (!root) return {};
+    vector<T> res;
+    auto cur = root; BinaryNode<T>* pre = nullptr;
+    while (cur) {
+        if (!cur->left) {  // 左子树不存在  访问+进入右节点
+            res.push_back(cur->data);
+            cur = cur->right;
+        } else {
+            // 判断前驱节点是否访问过: 是, 进入当前节点的右子树; 否 左子树
+            pre = cur->left;
+            //                      不判断 会死循环
+            while (pre->right && pre->right != cur) pre = pre->right;
+            if (!pre->right) { // 前驱节点未访问 存放后继节点
+                pre->right = cur;
+                cur = cur->left;
+            } else {           // 前驱节点访问过了 恢复树的结构
+                pre->right = nullptr;
+                res.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
+    }
+    return res;
+}
+
+template <class T>
+vector<T> PreorderMorris(BinaryNode<T> *root) {
+    if (!root) return {};
+    vector<T> res;
+    auto cur = root; BinaryNode<T>* pre = nullptr;
+    while (cur) {
+        if (!cur->left) {  // 左子树不存在  访问+进入右节点
+            res.push_back(cur->data);
+            cur = cur->right;
+        } else {
+            // 判断前驱节点是否访问过: 是, 进入当前节点的右子树; 否 左子树
+            pre = cur->left;
+            //                      不判断 会死循环
+            while (pre->right && pre->right != cur) pre = pre->right;
+            if (!pre->right) { // 前驱节点未访问 存放后继节点
+                res.push_back(cur->val);
+                pre->right = cur;
+                cur = cur->left;
+            } else {           // 前驱节点访问过了 恢复树的结构
+                pre->right = nullptr;
+                cur = cur->right;
+            }
+        }
+    }
+    return res;
+}
+
 
 template <class T>
 void Postorder(BinaryNode<T> *root) {
@@ -194,6 +284,33 @@ void PostorderNonRecursive2(BinaryNode<T> *root) {
     std::ostream_iterator<T> out_it (std::cout, " ");
     std::copy(res.begin(), res.end(), out_it);
 }
+
+// https://ghh3809.github.io/2018/08/06/morris-traversal/
+// 后续遍历的思想就是用栈 保存(根 右 左)
+// 由于后续遍历的回溯过程只可能上升一层，因此可以添加临时变量lastNode记录刚刚访问的节点，如果当前节点是上次访问节点的父节点，则说明子树访问完成，可以访问当前节点了。
+/*
+vector<int> postorderTraversal(TreeNode* root) {
+    if(!root) return {};
+    vector<int> vi;
+    stack<TreeNode*> st;
+    TreeNode* cur = nullptr;
+    st.push(root);
+    auto pre = root;
+    while (!st.empty()) {
+    auto cur = st.top(); st.pop();
+    //   叶子节点                           孩子节点已经访问过了
+    if((!cur->left && !cur->right) || (cur->left == pre || cur->right == pre)) {
+        vi.push_back(cur->val);
+        pre = cur;
+    } else {
+        st.push(cur);
+        if(cur->right) st.push(cur->right);
+        if(cur->left) st.push(cur->left);
+    }
+    }
+    return vi;
+}
+*/
 
 // https://www.cnblogs.com/grandyang/p/4051321.html
 // 层次遍历求平均值
@@ -620,12 +737,6 @@ void flatten(BinaryNode<T>* root) {
     root->right = tmp;
 }
 
-
-// time complexity: O(n)
-// space complexity:
-//     O(h)
-//     O(log n) -- best/avg
-//     O(n) -- worst
 int main() {
 	/* BST
 	            M
